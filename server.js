848f7a867a0bb7de0a4c4b716880440d2bcc7526
Joe Cooler <joe_cooler@msn.com>
2023-03-19 12:50:57 -0500

    na

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ router.get('/projects/', async (req, res) => {
     return
   }
   response.forEach(e => {
-    data.push({ id: e.id, title: e.title, info: e.info, open:e.open, closed:e.closed })
+    data.push({ id: e._id, title: e.title, info: e.info, open:e.open, closed:e.closed })
   })
   res.send(data)
 })
